09cfd413cc7b5984741e6be337a8c82076632aa2
Felipe Boba <felipe.borba0109@gmail.com>
2022-10-21 20:55:00 -0300

    feat: add database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const app = require("./app");
+const knex = require("./database/knex/index");
+const path = require("path");
+
+knex.migrate.latest({
+  directory: [path.resolve(__dirname, "users", "migrations"),]
+});
 
 const PORT = 3000; //TODO change to env var
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
