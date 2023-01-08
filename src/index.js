307c0e625b47cd2ba91a71079b04cfbc1dbf92d6
Felipe Borba <felipe.borba0109@gmail.com>
2023-01-08 14:37:06 -0300

    config deploy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require("dotenv/config");
 const app = require("./app");
-const migration = require('./database/knex/migration');
+const migration = require("./database/knex/migration");
 
 migration.latest();
 
 const PORT = process.env.PORT;
 if (!PORT) {
-  throw new Error('não esqueça no arquivo .env')
+  throw new Error("não esqueça no arquivo .env");
 }
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
